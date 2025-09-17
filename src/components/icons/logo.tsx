9fe23b08ae36ccb5a6e22902fff0f5f879663bae
kind012 <tranthienduc1001@gmal.com>
2025-09-17 09:56:05 +0700

    commit

---
+++ src/components/icons/logo.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <svg
+      id="Layer_2"
+      data-name="Layer 2"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 108.89 94.31"
+    >
+      <g id="Layer_1-2" data-name="Layer 1">
+        <g>
+          <path
+            fill="#fff"
+            fillRule="evenodd"
+            d="M45.38,62.87l-9.07,15.72-9.08,15.72h18.15l9.07-15.72,9.07-15.72,9.08-15.72h18.14l-9.07-15.72h-18.15l-9.07,15.72-9.07,15.72Z"
+          />
+          <path
+            fill="#fff"
+            fillRule="evenodd"
+            d="M81.67,62.87l-9.07,15.72-9.08,15.72h18.15s9.07-15.72,9.07-15.72h18.15l-9.07-15.72h-18.15Z"
+          />
+          <path
+            fill="#fff"
+            fillRule="evenodd"
+            d="M18.15,78.58l9.07-15.72,9.08-15.72,9.07-15.72,9.07-15.72h18.15L63.52,0h-18.14L0,78.58l9.08,15.72,9.07-15.72Z"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default Logo;
